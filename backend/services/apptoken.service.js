f844bfb68cde2acdcc05c3d7428d21558afecd21
Ricardo Rengifo <rengifor281@gmail.com>
2024-07-09 17:06:04 -0400

    Update appToken

---
+++ backend/services/apptoken.service.js
@@ -0,0 +1,20 @@
+
+/**
+ * Save a token
+ * @param {string} token
+ * @param {ObjectId} userId
+ * @param {Moment} expires
+ * @param {string} type
+ * @param {boolean} [blacklisted]
+ * @returns {Promise<Token>}
+ */
+const saveToken = async (token, userId, expires, type, blacklisted = false) => {
+  const tokenDoc = await Token.create({
+    token,
+    user: userId,
+    expires: expires.toDate(),
+    type,
+    blacklisted,
+  });
+  return tokenDoc;
+};
